2927b409a707659e98faa5a4d2d5f39e4b583921
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-10-11 15:35:59 +0200

    slett pollingapi siden det ikke er i bruk

--- src/main/kotlin/no/nav/personbruker/dittnav/varselbestiller/config/bootstrap.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/varselbestiller/config/bootstrap.kt
@@ -8,7 +8,6 @@ import io.ktor.features.DefaultHeaders
 import io.ktor.routing.routing
 import io.prometheus.client.hotspot.DefaultExports
 import kotlinx.coroutines.runBlocking
-import no.nav.personbruker.dittnav.varselbestiller.common.kafka.polling.pollingApi
 import no.nav.personbruker.dittnav.varselbestiller.health.healthApi
 
 fun Application.mainModule(appContext: ApplicationContext = ApplicationContext()) {
@@ -16,7 +15,6 @@ fun Application.mainModule(appContext: ApplicationContext = ApplicationContext()
     install(DefaultHeaders)
     routing {
         healthApi(appContext.healthService)
-        pollingApi(appContext)
 
         configureStartupHook(appContext)
         configureShutdownHook(appContext)
