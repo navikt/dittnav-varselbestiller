5cd16ba46e7297c51b33d43761aee5e03b2c2d45
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2024-01-30 12:43:07 +0100

    flytt varseltekst logikk til enum

--- src/main/kotlin/no/nav/tms/ekstern/varselbestiller/doknotifikasjon/varsel.kt
+++ src/main/kotlin/no/nav/tms/ekstern/varselbestiller/doknotifikasjon/varsel.kt
@@ -16,10 +16,24 @@ data class Produsent(
     val appnavn: String
 )
 
-enum class VarselType {
-    Oppgave,
-    Beskjed,
-    Innboks;
+enum class VarselType(val emailTitle: String, val smsText: String, emailTextFile: String) {
+    Oppgave(
+        emailTitle = "Du har fått en oppgave fra NAV",
+        smsText = "Hei! Du har fått en ny oppgave fra NAV. Logg inn på NAV for å se hva oppgaven gjelder. Vennlig hilsen NAV",
+        emailTextFile = "epost_oppgave.txt",
+    ),
+    Beskjed(
+        emailTitle = "Beskjed fra NAV",
+        smsText = "Hei! Du har fått en ny beskjed fra NAV. Logg inn på NAV for å se hva beskjeden gjelder. Vennlig hilsen NAV",
+        emailTextFile = "epost_beskjed.txt",
+    ),
+    Innboks(
+        emailTitle = "Du har fått en melding fra NAV",
+        smsText = "Hei! Du har fått en ny melding fra NAV. Logg inn på NAV for å lese meldingen. Vennlig hilsen NAV",
+        emailTextFile = "epost_innboks.txt",
+    );
+
+    val emailText = this::class.java.getResource("/texts/$emailTextFile")!!.readText(Charsets.UTF_8)
 
     @JsonValue
     fun lowercaseName() = name.lowercase()
